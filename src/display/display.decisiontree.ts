9b1f600b36139c3815a38d80a6b4e355c14d8bd1
bianchinmatteo99 <70894747+bianchinmatteo99@users.noreply.github.com>
2026-07-05 19:21:48 +0200

    Bug fix

--- src/display/display.decisiontree.ts
+++ src/display/display.decisiontree.ts
@@ -2,7 +2,7 @@ import { DecisionNode } from "../common/navigation/decisiontree";
 import { Page } from "../common/navigation/pages";
 import { QuizStatus } from "../common/quiz/quiz.model";
 import { DisplayStateHandler } from "./display.state";
-import { WaitingStartPage } from "./display.views";
+import { OnBoardingPage, WaitingStartPage } from "./display.views";
 
 export class DisplayRootPageChooser extends DecisionNode<DisplayStateHandler, Page> {
     name = "root"
@@ -14,6 +14,9 @@ export class DisplayRootPageChooser extends DecisionNode<DisplayStateHandler, Pa
         if (!state.read || state.read.app.quiz.status == QuizStatus.Booting || state.read.app.quiz.status == QuizStatus.AwaitingStart) {
             this.clearSubTree();
             return new WaitingStartPage();
+        } else if (state.read.app.quiz.status == QuizStatus.OnBoarding){
+            this.clearSubTree();
+            return new OnBoardingPage();
         } else {
             throw new Error("Unexpected state");
         }
